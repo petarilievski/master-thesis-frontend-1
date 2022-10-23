afff4e82ca3043a3b11e187c8a361648f54b0e86
Petar <petar@producercloud.com>
2022-10-23 22:30:11 +0200

    revert last working version

--- components/tabs/home-tab.tsx
+++ components/tabs/home-tab.tsx
@@ -1,21 +1,14 @@
 import Controls from "./subcomponents/home-tab/controls";
-import Screen from "./subcomponents/home-tab/VRScene.jsx";
 
 interface IProps {
     activeRun: boolean
 }
 
 const HomeTab = ({ activeRun }: IProps) => {
-    // var tag = document.createElement('script');
-    // tag.async = true;
-    // tag.src = 'THE PATH TO THE JS FILE OR A CDN LINK';
-    // var body = document.getElementsByTagName('body')[0];
-    // body.appendChild(tag);
 
     return (<>
         <div className={'flex flex-col justify-center items-center'}>
             <div className={'mb-4'}>
-                <Screen />
             </div>
             <Controls />
         </div>
