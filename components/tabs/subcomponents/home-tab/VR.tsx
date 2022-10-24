b380adbcee2ae7131939e08e83a57303c143d0d0
Petar <petar@producercloud.com>
2022-10-24 22:29:05 +0200

    connected to flask

--- components/tabs/subcomponents/home-tab/VR.tsx
+++ components/tabs/subcomponents/home-tab/VR.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import {useEffect} from "react";
 
 interface IProps {
--- components/tabs/subcomponents/home-tab/controls.tsx
+++ components/tabs/subcomponents/home-tab/controls.tsx
@@ -14,7 +14,17 @@ const Controls = ({emitData}: IProps) => {
     const [sliderValue, setSliderValue] = useState(0)
 
     useEffect(() => {
+        const data = JSON.stringify({
+            state: state,
+            tasterOne: tasterOne,
+            tasterTwo: tasterTwo,
+            buttonOne: buttonOne,
+            buttonTwo: buttonTwo,
+            sliderValue: sliderValue,
+        })
         emitData(state, tasterOne, tasterTwo, buttonOne, buttonTwo, sliderValue)
+        const fetchedData = fetch(`http://192.168.1.4:5001/buttons-status/${data}`, {method:'POST', mode: 'no-cors'})
+            .then(res => console.log(res))
     },[state, tasterOne, tasterTwo, buttonOne, buttonTwo, sliderValue])
     return (
         <>
